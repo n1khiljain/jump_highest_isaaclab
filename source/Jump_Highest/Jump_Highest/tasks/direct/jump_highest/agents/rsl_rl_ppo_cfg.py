540a8025806dd96b262b5cd487f61c8e1b640b8c
Nikhil Jain <njain110706@gmail.com>
2026-03-08 23:44:03 -0700

    modified max_iterations, hiddne layer sizes, and action_scale from 1 to 100 to jump

--- source/Jump_Highest/Jump_Highest/tasks/direct/jump_highest/agents/rsl_rl_ppo_cfg.py
+++ source/Jump_Highest/Jump_Highest/tasks/direct/jump_highest/agents/rsl_rl_ppo_cfg.py
@@ -11,15 +11,15 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 16
-    max_iterations = 500
+    max_iterations = 1000
     save_interval = 50
     experiment_name = "cartpole_direct"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
         actor_obs_normalization=False,
         critic_obs_normalization=False,
-        actor_hidden_dims=[32, 32],
-        critic_hidden_dims=[32, 32],
+        actor_hidden_dims=[256, 128, 64],
+        critic_hidden_dims=[256, 128, 64],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
--- source/Jump_Highest/Jump_Highest/tasks/direct/jump_highest/jump_highest_env_cfg.py
+++ source/Jump_Highest/Jump_Highest/tasks/direct/jump_highest/jump_highest_env_cfg.py
@@ -22,7 +22,7 @@ class JumpHighestEnvCfg(DirectRLEnvCfg):
     action_space = 21
     observation_space = 54  # 1 + 4 + 3 + 3 + 21 + 21
     state_space = 0
-    action_scale = 1.0
+    action_scale = 100.0
 
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
@@ -35,10 +35,10 @@ class JumpHighestEnvCfg(DirectRLEnvCfg):
 
     # jump reward
     rew_scale_jump_vel = 0.5        # reward upward velocity
-    rew_scale_height = 1.0          # reward height above ground
-    rew_scale_alive = 2.0           # reward for not falling
-    rew_scale_upright = 1.0         # reward for staying upright
-    rew_scale_termination = -2.0    # penalty for falling
+    rew_scale_height = 0.5          # reward height above ground
+    rew_scale_alive = 5.0           # reward for not falling
+    rew_scale_upright = 2.0         # reward for staying upright
+    rew_scale_termination = -5.0    # penalty for falling
 
     # termination
     min_height = 0.5 # reset if root drops below this 
